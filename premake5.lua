e224e3950e2bf4c841dfcf35fd7c4a3e249020f7
DogWealth <liuyang_16529@163.com>
2023-10-13 14:11:34 +0800

    added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,109 @@
+workspace "PIEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "PIEngine"
+	location "PIEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir("bin/" ..outputdir .. "/%{prj.name}")
+	objdir("bin-int/" ..outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"PI_PLATFORM_WINDOWS",
+			"PI_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+
+	filter "configurations:Debug"
+		defines "PI_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PI_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PI_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/" ..outputdir .. "/%{prj.name}")
+	objdir("bin-int/" ..outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"PIEngine/vendor/spdlog/include",
+		"PIEngine/src"
+	}
+
+	links
+	{
+		"PIEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"PI_PLATFORM_WINDOWS",
+		}
+
+
+	filter "configurations:Debug"
+		defines "PI_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PI_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PI_DIST"
+	
